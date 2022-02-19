d6d9024b6b0a4ca068d162daaac4c4a1906e52b1
Animesh384 <68201762+Animesh384@users.noreply.github.com>
2022-02-19 19:54:15 +0530

    Create sighin.php
    sign in page

---
+++ html/sighin.php
@@ -0,0 +1,46 @@
+<html>
+<head>   
+<title>sign in</title>   
+
+
+
+</head>
+
+
+
+<body>
+
+
+<form action="sighin.php" method="get"> 
+
+
+
+
+
+
+  </form>
+
+<table border="2">
+
+<tr>
+<td>user id</td>
+<td><input type="text" name="id">   </td>
+
+
+</tr>
+</tr>
+<td>password</td>
+<td><input type="text" name="pass">   </td>
+
+
+</tr>
+</table>
+<br>
+
+<input type="submit" name="submit" value="submit"/>
+<br>
+
+</body>
+
+
+</html>
