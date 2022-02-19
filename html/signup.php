4e7ff8aa2025d3b56ae7f6240fc6b295120cc216
Animesh384 <68201762+Animesh384@users.noreply.github.com>
2022-02-19 19:52:47 +0530

    Create signup.php
    signup page

---
+++ html/signup.php
@@ -0,0 +1,92 @@
+<html>
+    <head>
+     <title>signin </title>
+    </head>
+
+
+
+<body>
+<form>
+<table border="2">
+
+<tr>
+<td>user name</td>
+<td><input type="text" name="fn">   </td>
+
+
+</tr>
+</tr>
+<td>user id</td>
+<td><input type="text" name="user">   </td>
+
+
+</tr>
+
+<tr>
+<td>password</td>
+<td><input type="text" name="pass">   </td>
+
+
+</tr>
+<tr>
+<td>conform password</td>
+<td><input type="text" name="cpass">   </td>
+
+</tr>
+</table>
+
+</form>
+<p>select the programing language you  know:      </p>
+
+    <input type="checkbox"  name="php"><strong>php</strong>
+     <input type="checkbox"  name="c++"><strong>c++</strong>
+     <input type="checkbox"  name="html"><strong>html</strong>
+     <input type="checkbox"  name="python"><strong>python</strong>
+     <input type="checkbox"  name="php"><strong>mysql</strong>
+     <input type="checkbox"  name="c++"><strong>java</strong>
+     <input type="checkbox"  name="html"><strong>R</strong>
+    <input type="checkbox"  name="python"><strong>css</strong>
+
+<br>
+
+about yourself:<br>
+<textarea rows="5" cols="50" name="des">
+</textarea>
+<br>
+Gender:
+<input type="radio" name="G" value="m">M
+<input type="radio" name="G" value="f">F
+<br>
+
+department:
+
+
+<select name="drop">
+<option  value="IT">IT</option>
+<option value="Finace" > Finance</option>
+<option value="Marketing">marketing </option>
+<option value="Human Resorce">Human Resorce</option>
+</select>
+
+<br>
+your CV:
+<input type="file" name=fileu" accept="pdf/*"/>
+
+
+<br>
+
+<input type="submit" name="submit" value="submit"/>
+<br>
+
+
+
+
+
+   
+
+
+
+
+</body>
+
+ </html>
